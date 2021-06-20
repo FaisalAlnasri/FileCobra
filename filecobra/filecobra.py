c0a3f0a08f20c1d0e32070179ef8ed1efacf584c
FaisalAlnasri <faisal.alnasri130@gmail.com>
2021-06-20 19:37:37 +0300

    created extra functionalty to the 'create' function:
    added 'overwrite' and 'contents' arguments to the function
    
    fixed syntax errors
    
    improved functions documentation

--- filecobra/filecobra.py
+++ filecobra/filecobra.py
@@ -1,29 +1,28 @@
 import os
 from errors import *
 
-def create(url: string, is_dir: bool=False):
+def create(url, is_dir = False, overwrite=False, contents = ''):
     """
     adds a new empty file/directory in a specifed path
     ------------------
-        create(url, is_dir=False)
-    ------------------
-    url: string -> path of the file
-    is_dir: bool -> True if you want to make a directory, False by default
+    url -> path of the file
+    is_dir -> True if you want to make a directory, False by default
+    overwrite -> True if you want to overwrite existing file.
+    contents -> the contents you want the file to have
     """
     if is_dir:
         if not os.path.isdir(url):
             os.mkdir(url)
     else:
-        if not os.path.isfile(url):
-            open(url, 'w')
+        if not os.path.isfile(url) or overwrite:
+            f = open(url, 'w')
+            f.write(contents)
 
-def delete(url: string):
+def delete(url):
     """
     deletes a file/directory in a specifed path
     ------------------
-        delete(url)
-    ------------------
-    url: string -> path of the file
+    url -> path of the file
     """
     if os.path.isfile(url):
         os.remove(url)
@@ -31,28 +30,24 @@ def delete(url: string):
         try:
             os.rmdir(url)
         except OSError:
-            raise DirectoryNotEmpty("cant delete directory")
+            raise DirectoryNotEmpty("cant delete directory, must be empty")
 
-def move(url: string, new_url: string):
+def move(url, new_url):
     """
     moves file to a new location
     ------------------
-        move(old_path, new_path, filename)
-    ------------------
-    url: string -> current path of the file
-    new_url: string -> new path of the file
+    url -> current path of the file
+    new_url -> new path of the file
     """
     if not os.path.isfile(new_url):
         os.rename(url, new_url)
 
-def rename(url: string, new_name: string):
-    """
+def rename(url, new_name):
+    """]
     moves file to a new location
     ------------------
-        move(old_path, new_path, filename)
-    ------------------
-    url: string -> path of the file
-    new_name: string -> the name you want the file to be
+    url -> path of the file
+    new_name -> the name you want the file to be
     """
     url_path_names = url.split('\\')
     if len(url_path_names) >= 2:
@@ -63,15 +58,14 @@ def rename(url: string, new_name: string):
     if os.path.isfile(url):
         os.rename(url, name)
 
-def check(url: string) -> bool:
+def check(url) -> bool:
     """
     return True if a file/directorys exists
     ------------------
-        check(url)
-    ------------------
-    url: string -> path of the file
+    url -> path of the file
     """
     return os.path.isfile(url) or os.path.isdir(url)
 
 if __name__ == "__main__":
-    delete('test')
+    # for testing purposes
+    pass
